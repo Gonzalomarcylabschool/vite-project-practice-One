da2172ee5a8bdb429d0397d429285eb752dd3980
gonzalorportfolio <gromeroeducation@gmail.com>
2023-12-06 14:39:38 -0500

    update

--- app/src/main.js
+++ app/src/main.js
@@ -1,1 +1,36 @@
 import './style.css'
+import { getEl } from './mods/utils';
+
+const handelSubmit = (e) => {
+  e.preventDefault();
+  const formObj = Object.fromEntries(new FormData(e.target));
+  console.log(formObj);
+  addValue(formObj);
+}
+
+const setLocalStorageKey = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+const getLocalStorageKey = (key) => {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+export const getValue = (key) => getLocalStorageKey(key);
+export const setValue = (key, names) => setLocalStorageKey(key, names);
+
+const addValue = (value) => {
+  const values = getValue('form-data');
+  setValue('form-data',[...values, value]);
+}
+const main = () => {
+
+  getEl('#movie-input').addEventListener('submit', handelSubmit );
+  console.log(getValue('form-data'));
+}
+
+main();
---
+++ app/src/mods/utils.js
@@ -0,0 +1,1 @@
+export const getEl = (element) => document.querySelector(element);
